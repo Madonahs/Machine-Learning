6eeafe8b05cc3200471237f71ce16805193b2dab
Madonahs <syombuamadona@gmail.com>
2018-03-18 09:31:38 -0500

    simple search name madonah

---
+++ MachineLearning/src/com/madonahsyombua/ml/SimpleNameSearch.java
@@ -0,0 +1,61 @@
+package com.madonahsyombua.ml;
+
+import java.util.ArrayList;
+import java.util.List;
+import net.sf.classifier4J.ClassifierException;
+import net.sf.classifier4J.vector.HashMapTermVectorStorage;
+import net.sf.classifier4J.vector.TermVectorStorage;
+import net.sf.classifier4J.vector.VectorClassifier;
+
+public class SimpleNameSearch {
+
+	public SimpleNameSearch() {
+		
+		List<String> name = new ArrayList<>();
+		
+		name.add("madonah");
+		name.add("madona");
+		name.add("madonna");
+		name.add("maddona");
+		name.add("maddoonna");
+		name.add("madonahhh");
+		name.add("madonayyy");
+		name.add("madonnauuu");
+		name.add("maddonattt");
+		name.add("maddoonnaiii");
+		
+		
+		TermVectorStorage storage = new HashMapTermVectorStorage();
+		VectorClassifier vc = new VectorClassifier(storage);
+		
+		String correctName = "madonah";
+		
+		for(String names: name) {
+			try {
+				
+				vc.teachMatch("sterm",correctName);
+				
+				double results = vc.classify("sterm", names);
+			
+				System.out.println(names + "=" + results);
+				
+				
+			}catch(ClassifierException e) {
+				
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		SimpleNameSearch ns = new SimpleNameSearch();
+		
+	}
+	
+}
