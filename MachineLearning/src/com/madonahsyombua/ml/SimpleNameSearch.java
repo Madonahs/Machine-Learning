6cf7eb028c1c0c76fe4a9a53376832030cf04f7a
Madonahs <syombuamadona@gmail.com>
2018-03-22 22:01:29 -0500

    fixed #7

---
+++ MachineLearning/src/com/madonahsyombua/ml/SimpleNameSearch.java
@@ -0,0 +1,63 @@
+package com.madonahsyombua.ml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.classifier4J.ClassifierException;
+import net.sf.classifier4J.vector.HashMapTermVectorStorage;
+import net.sf.classifier4J.vector.TermVectorStorage;
+import net.sf.classifier4J.vector.VectorClassifier;
+
+public class SimpleNameSearch {
+
+    public static void parametrizedSimpleSearch(List<String> searchList, String wordToSearch) {
+
+        TermVectorStorage storage = new HashMapTermVectorStorage();
+        VectorClassifier vc = new VectorClassifier(storage);
+
+        for (String element : searchList) {
+            try {
+
+                vc.teachMatch("sterm", wordToSearch);
+
+                double results = vc.classify("sterm", element);
+
+                System.out.println(element + " = " + results);
+
+            } catch (ClassifierException e) {
+
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        SimpleNameSearch ns = new SimpleNameSearch();
+        System.out.println("Machine Learning: ");
+        System.out.println("******************************************");
+        List<String> names = new ArrayList<String>();
+
+        names.add("nida");
+        names.add("nidu");
+        names.add("nidha");
+        names.add("rhizlane");
+        names.add("niiddaa");
+        names.add("needa");
+        names.add("madonah");
+        names.add("nidaa");
+        names.add("madonnaah");
+
+     
+        parametrizedSimpleSearch(names,"nida");
+
+        System.out.println("******************************************");
+        parametrizedSimpleSearch(names,"madonah");
+
+        System.out.println("******************************************");
+        parametrizedSimpleSearch(names,"rhizlane");
+
+    }
+
+}
