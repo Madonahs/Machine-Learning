82f1ad74803be8162cb60e409e88e6ffe2f3af55
Madonahs <syombuamadona@gmail.com>
2018-04-03 00:22:50 -0500

    issue 13 breakdown

--- NameSearch/src/Matrix/exercie.py
+++ NameSearch/src/Matrix/exercie.py
@@ -1,8 +1,34 @@
 import tensorflow as tf
-import numpy as np
 
+'''
+This exercise is based on issue #13 please check the issue on our Board.
+Problem approach using tensor flow:
+So given the following X^TW1W2W3
+X=[1,2,3]
+W1 = [2,0,1,0,1,2,3,0,1]
+W2 = [1,0,1,2,2,1,0,3,0]
+W3 =[2,4,1]
 
+X = [1]
+    [2]
+    [3]
+    
+W1 = [2,0,1]
+     [0,1,2]
+     [3,0,1]
+     
+W2 = [1,0,1]
+     [2,2,1]
+     [0,3,0]
+     
+W3 = [2]
+     [4]
+     [1]
+Always remember RC| Row and Columns 
+We know X transpose will change from column to row; hence
 
+Soln = [155]
+'''
 print("Exercise")
 X = tf.constant([1, 2, 3], shape=[3, 1])
 W1 = tf.constant([2, 0, 1, 0, 1, 2, 3, 0, 1], shape=[3, 3])
