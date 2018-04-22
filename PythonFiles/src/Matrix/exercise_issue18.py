d3022372063287f8a28d801127c486b1bdd5fd7c
Madonahs <syombuamadona@gmail.com>
2018-04-22 10:38:40 -0500

    fixed #18

--- PythonFiles/src/Matrix/exercise_issue18.py
+++ PythonFiles/src/Matrix/exercise_issue18.py
@@ -166,4 +166,14 @@ print("\n")
 print("Final Solution", sess.run(K) - Y_2)
 
 
+'''
+Here is a nice play ground
 
+https://playground.tensorflow.org/#activation=tanh&batchSize=10&dataset
+=xor&regDataset=reg-plane&learningRate=0.03&regularizationRate=
+0&noise=0&networkShape=4,4,4&seed=0.37235&showTestData=
+false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=
+false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=
+false&collectStats=false&problem=classification&initZero=false&hideText=false
+
+'''
