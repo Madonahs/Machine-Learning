14d44a06e0b4e8f9386b4eb1739e51bf2146a871
Madonahs <syombuamadona@gmail.com>
2018-08-15 14:25:18 -0500

    add licence

--- SimCardSwap/SimSwap.py
+++ SimCardSwap/SimSwap.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2018 Madonah Syombua
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 Created on Thu Aug  9 17:35:26 2018
 
@@ -128,6 +140,7 @@ Id	Age 	No. of Complaints	 MP in Ksh	Contacts	Swap Agent
 29	33  	2	                  3000	        10	           1
 30	45	   1	                  1200	         50	            1
 '''
+
 
 
 
