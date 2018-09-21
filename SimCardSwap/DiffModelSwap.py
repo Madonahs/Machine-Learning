4923a11c03f19f8beb433d4920f64d9787ca8a71
Madonahs <syombuamadona@gmail.com>
2018-09-21 16:02:41 -0500

    Naive Bayes

---
+++ SimCardSwap/DiffModelSwap.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 Madonah Syombua
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Created on Fri Sep 21 15:58:50 2018
+
+@author: madon
+
+I will try using a  different model
+Machine learning algorithms:
+Linear Algorithms:
+Algorithm 1: Linear Regression
+Algorithm 2: Logistic Regression
+Algorithm 3: Linear Discriminant Analysis
+*Nonlinear Algorithms:
+
+Algorithm 4: Classification and Regression Trees
+Algorithm 5: Naive Bayes
+Algorithm 6: K-Nearest Neighbors
+Algorithm 7: Learning Vector Quantization
+Algorithm 8: Support Vector Machines
+*Ensemble Algorithms:
+
+Algorithm 9: Bagged Decision Trees and Random Forest
+Algorithm 10: Boosting and AdaBoost
+"""
+
